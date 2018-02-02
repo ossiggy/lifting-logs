3d43804c3dc6d8adc1f4fe367a525f30dc17a182
Ossiggy <mrossig201@gmail.com>
2018-02-02 09:04:12 -0500

    beta up and running, more features to be added

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 'use strict';
 
-// have user choose what muscle group they worked on
-// legs chest etc
 // have some preloaded exercises with the option to add more
-// have an input for weight, reps
-// calculate the amount of weight per exercise
-// calculate total weight moved
 
 $('.muscle-group').on('change', generateExercise);
-$('.add-it').on('click', addSet);
+$('.add-set').on('click', addSet);
+$('.undo').on('click', removeSet);
+
+let totalWeight = 0;
+let workout = [];
 
 function generateExercise(event){
   event.preventDefault();
@@ -23,14 +22,78 @@ function generateExercise(event){
       });
     }).show();
   });
+
   $('.user-input').show();
-  $('.add-it').show();
+  $('.add-set').show();
 }
 
 function addSet(event){
   event.preventDefault();
+
+  const group = $('.muscle-group').find(':selected').val();
+  const exercise = $('.exercise').find(':selected').val();
   const weight = $('.weight').val();
   const reps = $('.reps').val();
 
+  const set = {
+    group: group,
+    exercise: exercise,
+    weight: weight,
+    reps: reps
+  };
+
+  if(!weight||!reps){
+    alert('Cannot Be 0');
+    return;
+  }
   
+  $('.workout').append(
+    `<div class='result'>${exercise}: ${weight}lbs for ${reps} reps</div>`
+  ).show();
+
+  retrieveSetData();
+  buildWorkOut(set);
+
+}
+
+function buildWorkOut(set){
+  workout.push(set);
+}
+
+function retrieveSetData(){
+  let liftData;
+  
+  const children = $('.workout').children('.result');
+
+  for(let i=0; i<children.length; i++){
+    liftData = children[i].innerHTML.match(/(\d+)/g);
+  }
+
+  const setWeight = liftData[0]*liftData[1];
+
+  totalWeight = totalWeight + setWeight;
+
+  showTotalWeight();
+}
+
+function showTotalWeight(){
+  $('.total').html(totalWeight+'lbs moved');
+}
+
+function removeSet(event){
+  event.preventDefault();
+  const child = $('.workout').children('.result').last();
+  
+  for(let i=0; i<child.length; i++){
+    const liftData = child[i].innerHTML.match(/(\d+)/g);
+    const setWeight = liftData[0]*liftData[1];
+    totalWeight = totalWeight - setWeight;
+  }
+
+  child.remove();
+  workout.pop();
+  showTotalWeight();
+  if(totalWeight===0){
+    $('.total').html('');
+  }
 }
